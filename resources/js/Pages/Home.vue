535e90323ac30daf394487816a339e41988c3631
BeQQaa <xjustted@gmail.com>
2022-04-01 07:13:42 +0300

    init project Laravel JetStream (Inertia-Vue)

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>{{title}}</h1>
+</template>
+
+<script>
+import NavBar from "../Layout/NavBar";
+export default {
+    components: {NavBar},
+    props:{
+        title:String
+    }
+}
+</script>
